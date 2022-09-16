631ae828356e042adb2f800dffc4d756d48af650
Shaban27 <l-shaban@yandex.ru>
2022-09-17 00:41:11 +0900

    Переделанная курсова

--- src/Employee.java
+++ src/Employee.java
@@ -1,32 +1,20 @@
 public class Employee {
-    private static int counter = 0;
-    private final String name;
-    private final String lastname;
-    private final String surname;
+       private static int counter = 0;
+    private final String FIO;
     private int department;
-    private int salary;
+    private float salary;
     private final Integer id;
 
 
     // Геттеры
-    public String getName() {
-        return name;
+    public String getFIO() {
+        return FIO;
     }
-
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
     public int getDepartment() {
         return department;
     }
 
-    public int getSalary() {
+    public float getSalary() {
         return salary;
     }
 
@@ -43,23 +31,22 @@ public class Employee {
         this.department = department;
     }
 
-    public void setSalary(int salary) {
+    public void setSalary(float salary) {
         this.salary = salary;
     }
 
-    public Employee(String name, String lastname, String surname, int department, int salary) {
-        this.name = name;
-        this.lastname = lastname;
-        this.surname = surname;
+    public Employee(String FIO, int department, int salary) {
+        this.FIO = FIO;
         this.department = department;
         this.salary = salary;
         this.id = counter++;
     }
 
+
     @Override
     public String toString() {
-        return "Employee{" + "name='" + name + '\'' + ", lastname='" + lastname + '\'' + ", surname='" + surname + '\'' + ", department=" + department + ", salary=" + salary + ", id=" + id + '}';
-    }
+        return "Employee{" + "FIO='" + FIO + '\'' + ", department=" + department + ", salary=" + salary + ", id=" + id + '}';
+    }}
 
 
-    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,93 @@
-import java.net.ProxySelector;
-import java.util.Arrays;
-import java.util.IntSummaryStatistics;
-import java.util.Iterator;
-import java.util.stream.Collectors;
-
 public class Main {
+
     public static void main(String[] args) {
-        Employee[] employee = new Employee[10];
-        employee[0] = new Employee("Ольга", " Олеговна", " Спицына", 1, 200);
-        employee[1] = new Employee("Виктори", " Игоревна", " Матыш", 1, 100);
-        employee[2] = new Employee("Елена", " Ботсовна", " Бабич", 1, 300);
-        employee[3] = new Employee("Светлана", " Васильевна", " Берестнева", 1, 400);
-        employee[4] = new Employee("Алексадр", " Юрьевич", " Попов", 1, 500);
-        employee[5] = new Employee("Аллексей", " Иванович", " Иванов", 1, 600);
-        employee[6] = new Employee("Леонид", " Ботсович", " Евглевский", 1, 700);
-        employee[7] = new Employee("Ольга", " Ивановна", " Воз", 1, 800);
-        employee[8] = new Employee("Таитьяна", " Леонидовна", " Петрова", 1, 900);
-        employee[9] = new Employee("Дмитрий", " Александрович", " Решетников", 1, 50);
 
+        Employee[] employees = new Employee[10];
+        Employee employee1 = new Employee("Виктори Игоревна Матыш", 1, 200);
+        Employee employee2 = new Employee("Елена Борисовна Бабич", 1, 3000);
+        Employee employee3 = new Employee("Светлана Васильевна Берестнева", 1, 1000);
+        Employee employee4 = new Employee("Алексадр Юрьевич Попов", 1, 4000);
+        Employee employee5 = new Employee("Аллексей Иванович Иванов", 1, 500);
+        Employee employee6 = new Employee("Леонид Ботсович Евглевский", 1, 700);
+        Employee employee7 = new Employee("Ольга Ивановна Воз", 1, 800);
+        Employee employee8 = new Employee("Таитьяна Леонидовна Петрова", 1, 600);
+        Employee employee9 = new Employee("Дмитрий Александрович Решетников", 1, 700);
 
-        for (Employee e : employee) {
-            System.out.println(e);
+        employees[0] = employee1;
+        employees[1] = employee2;
+        employees[2] = employee3;
+        employees[3] = employee4;
+        employees[4] = employee5;
+        employees[5] = employee6;
+        employees[6] = employee7;
+        employees[7] = employee8;
+        employees[8] = employee9;
 
-            System.out.println("Ф.И.О  - " + e.getName() + e.getLastname() + e.getSurname());
+        printStatikMetod(employees);
+        printMinSalary(employees);
+        printMaxSalary(employees);
+        printListEmployees(employees);
+    }
+
+    private static void printStatikMetod(Employee[] employees) {
+        double summ = 0;
+        int countOfEmployees = 0;
+        int employeeNumber = 1;
+        if (employees == null) {
+            System.out.println("Если пусто");
+            return;
+        } else {
+            for (int i = 0; i < employees.length; i++) {
+                if (employees[i] != null) {
+                    System.out.println("Сотрудник" + employeeNumber + " - " + employees[i].toString());
+                    summ += employees[i].getSalary();
+                    countOfEmployees++;
+                    employeeNumber++;
+                }
+            }
+            System.out.println("----------------");
+            System.out.print("Cумма затрат на зарплаты в месяц: ");
+            System.out.printf(" %.2f\n", summ);
+            System.out.println("----------------");
+            System.out.print("Среднее значение зарплат : ");
+            System.out.printf(" %.2f\n", (summ / countOfEmployees));
+            System.out.println("");
         }
-        IntSummaryStatistics result = Arrays.stream(employee).collect(Collectors.summarizingInt(Employee::getSalary));
-        System.out.println(result.getSum());
-        System.out.println(result.getMin());
-        System.out.println(result.getMax());
-        System.out.println(result.getAverage());
+    }
+
+    private static void printMinSalary(Employee[] employees) {
+        float minSalary = employees[0].getSalary();
+        int indexEmployee = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].getSalary() < minSalary) {
+                minSalary = employees[i].getSalary();
+                indexEmployee = i;
+            }
+        }
+        System.out.println("Минимальная зарплата составляет: " + minSalary);
 
     }
 
+    private static void printMaxSalary(Employee[] employees) {
+        float maxSalary = employees[0].getSalary();
+        int indexEmployee = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].getSalary() > maxSalary) {
+                maxSalary = employees[i].getSalary();
+                indexEmployee = i;
+            }
+        }
+        System.out.println("________________");
+        System.out.println("Максимальная зарплата составляет: " + maxSalary);
+    }
+
+    private static void printListEmployees(Employee[] employees) {
+        System.out.println("Ф.И.О. сотрудников:");
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                System.out.println(employees[i].getFIO());
+
+            }
+        }
+    }
 }
-
-
-
-
-
-
